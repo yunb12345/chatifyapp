bf2cad83a1c0193403bce312c65d1be4c04e7f75
yunb12345 <agusyoon11@hotmail.com>
2025-12-29 17:27:57 -0300

    endpoint de check para verficar sesion

--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { signup,login,logout,updateProfile} from '../controllers/authController.js';
+import { protectRoute } from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
 
@@ -7,5 +8,6 @@ router.post('/signup',signup);
 router.post('/login',login);
 router.post('/logout',logout);
 router.post('update-profile',protectRoute,updateProfile)
+router.get('/check',protectRoute,(req,res) => res.status(200).json(req.user));
 
 export default router;
