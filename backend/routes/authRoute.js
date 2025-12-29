bde00b5b00601daae997b68d39e477e781232fcd
yunb12345 <agusyoon11@hotmail.com>
2025-12-29 16:58:58 -0300

    implementacion con cloudinary, instalacion de cookieparser y cambios a la estructura

--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { signup,login,logout} from '../controllers/authController.js';
+import { signup,login,logout,updateProfile} from '../controllers/authController.js';
 
 const router = express.Router();
 
 router.post('/signup',signup);
 router.post('/login',login);
 router.post('/logout',logout);
+router.post('update-profile',protectRoute,updateProfile)
 
 export default router;
