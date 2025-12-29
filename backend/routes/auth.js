626902c2b66927b8a814755a214ae5d60b4c7d8b
yunb12345 <agusyoon11@hotmail.com>
2025-12-29 15:53:20 -0300

    funcion de login y logout

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,8 +1,10 @@
 import express from 'express';
-import { signup } from '../controllers/authController.js';
+import { signup,login,logout} from '../controllers/authController.js';
 
 const router = express.Router();
 
 router.post('/signup',signup);
+router.post('/login',login);
+router.post('/logout',logout);
 
 export default router;
