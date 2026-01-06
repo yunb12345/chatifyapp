99cd2d0e3940e68809b46b49f5d9a1a1a7197f81
yunb12345 <agusyoon11@hotmail.com>
2026-01-05 21:45:23 -0300

    implementacion de cors en el back y en el front axios y zustand para consumir api y gestionar estado global

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import path from "path";
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
 import authRoute from './routes/authRoute.js';
 import messageRoute from './routes/messageRoute.js';
@@ -12,6 +13,7 @@ dotenv.config();
 const app = express();
 const __dirname = path.resolve();
 app.use(express.json());
+app.use(cors({origin: process.env.CLIENT_URL, credentials:true}));
 app.use(express.urlencoded({
     extended: true
 }));
