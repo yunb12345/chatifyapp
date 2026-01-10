345c66033cb1ea7719ed767b2d79ef8214211f5a
yunb12345 <agusyoon11@hotmail.com>
2026-01-10 19:11:17 -0300

    fix de error typo en cloudinary backend e implementacion de subir img en el front

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ import {connectDB} from './db/db.js';
 dotenv.config();
 const app = express();
 const __dirname = path.resolve();
-app.use(express.json());
+app.use(express.json({limit:'5mb'}));
 app.use(cors({origin: process.env.CLIENT_URL, credentials:true}));
 app.use(express.urlencoded({
     extended: true
--- backend/utils/cloudinary.js
+++ backend/utils/cloudinary.js
@@ -3,8 +3,8 @@ import dotenv from "dotenv";
 
 dotenv.config()
 cloudinary.config({
-    cloud_name:process.env.CLODINARY_CLOUD_NAME,
-    api_key : process.env.CLODINARY_API_KEY,
+    cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
+    api_key : process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET
 })
 
