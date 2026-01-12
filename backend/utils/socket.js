41389d7c2a0b88a546ddac5036d0e5cf858a1608
yunb12345 <agusyoon11@hotmail.com>
2026-01-12 16:44:34 -0300

    integracion con socketio en el back

---
+++ backend/utils/socket.js
@@ -0,0 +1,38 @@
+import {Server} from 'socket.io';
+import http from 'http';
+import express from 'express';
+import dotenv from 'dotenv';
+import { socketAuthMiddleware } from '../middlewares/socket.auth.middleware.js';
+
+const app = express();
+const server = http.createServer(app);
+
+const io = new Server(server,{
+    cors:{
+        origin: process.env.CLIENT_URL,
+        credentials:true,
+    },
+});
+
+io.use(socketAuthMiddleware);
+
+//esto es para almacenar los usuarios online
+const userSocketMap = {}; // {userId: socketId}
+
+io.on('connection', (socket) => {
+    console.log("Usuario conectado",socket.user.fullName);
+
+    const userId = socket.userId;
+    userSocketMap[userId] = socket.id;
+    
+    //io.emit es usado para enviar a todos los clientes conectados
+    io.emit("getOnlineUsers",Object.keys(userSocketMap));
+
+    socket.on("disconnect", () => {
+        console.log("Usuario desconectado",socket.user.fullName);
+        delete userSocketMap[userId];
+        io.emit("getOnlineUsers",Object.keys(userSocketMap));
+    });
+});
+
+export {io,app,server}
