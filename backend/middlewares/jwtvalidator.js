a1bf5e216453b472f59884daf0685c6a46f10f75
yunb12345 <agusyoon11@hotmail.com>
2026-01-23 18:50:20 -0300

    fix de tipeo en sameSite al generar el token

--- backend/middlewares/jwtvalidator.js
+++ backend/middlewares/jwtvalidator.js
@@ -5,7 +5,7 @@ export const generateToken = (userId,res) => {
     res.cookie("jwt",token,{
         maxAge: 7*24*60*60*1000, // esta en milisegundos
         httpOnly: true, //esto es para prevenir XSS
-        sameSite: "none", //previene CSRF || lo cambie porque ahora estan por separado damn
+        sameSite: "None", //previene CSRF || lo cambie porque ahora estan por separado damn
         secure: process.env.NODE_ENV === "development" ? false:true, //si no es development el campo es falso
     });
     return token;
