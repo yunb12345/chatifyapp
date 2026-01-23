a39cbaeceb3985dc3912557ab36015de0c5d6888
yunb12345 <agusyoon11@hotmail.com>
2026-01-23 18:21:28 -0300

    ahora el token no espera que venga del propio sitio y la baseURL del io client esta cambiada

--- frontend/src/store/useAuthStore.jsx
+++ frontend/src/store/useAuthStore.jsx
@@ -3,7 +3,7 @@ import {axiosInstance} from "../lib/axios.js";
 import toast from "react-hot-toast";
 import {io} from "socket.io-client";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:3000" : "/";
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:3000" : "https://chatifyapp-68jw.onrender.com";
 
 export const useAuthStore = create((set,get)=>({ //la variable set sirve para actualizar los estados
     authUser:null,
